5c1f9331017f2cb719b6411e7c1fa54617184902
Deepak Rikhari (171867) <deeparik@amdocs.com>
2021-01-08 00:01:12 +0530

    Ready for HEROKU Deployment

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,7 +8,7 @@ weatherForm.addEventListener('submit', (e) => {
     const location = searched.value;
     p1.textContent = 'Loading...';
     p2.textContent = '';
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if(data.error)
                 return p1.textContent = data.error;
